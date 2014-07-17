c1f1f0e501a415203915b2698120900987ab9c1f
Olivier Louvignes <olivier@mg-crea.com>
2014-07-17 17:50:01 +0200

    refactor(gulp): externalize ngFactory tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,114 +1,16 @@
 'use strict';
 
 var gulp = require('gulp');
-var _assign = require('lodash.assign');
-var $ = require('gulp-load-plugins')();
+var run = require('run-sequence');
 
-//////////////////////////////////////////////////////////////////////////////
-// TASK
-//////////////////////////////////////////////////////////////////////////////
+// Load ngFactory tasks
+require('gulp/index.js');
+// var config = require('gulp/config.js');
 
-gulp.task('default', ['jshint', 'karma']);
-gulp.task('serve', ['dist']);
-gulp.task('dist', ['uglify']);
+gulp.task('build', ['dist']);
 
-//////////////////////////////////////////////////////////////////////////////
-// KARMA
-//////////////////////////////////////////////////////////////////////////////
-
-
-function testConfig(configFile, customOptions) {
-  var options = { configFile: configFile };
-  var travisOptions = process.env.TRAVIS && { browsers: [ 'Firefox', 'PhantomJS'], reporters: ['dots'], singleRun: true  };
-  return _assign(options, customOptions, travisOptions);
-}
-
-
-gulp.task('karma', function () {
-
-});
-
-
-
-//////////////////////////////////////////////////////////////////////////////
-// OTHER
-//////////////////////////////////////////////////////////////////////////////
-
-gulp.task('ng-annotate', function () {
-  return gulp.src('*.js', { cwd: './src' })
-    .pipe($.changed('./dist'))
-    .pipe($.ngAnnotate())
-    .pipe(gulp.dest('./dist'));
-});
-
-var browserSync = require('browser-sync');
-function startBs() {
-  return browserSync.init(null, {
-    server: {
-      open: false,
-      debounce: 200,
-      baseDir: "./out/built/gh-pages/"
-    }
-  });
-}
-gulp.task('browser-sync', function () {
-  startBs();
-});
-
-
-//////////////////////////////////////////////////////////////////////////////
-// LINTING
-//////////////////////////////////////////////////////////////////////////////
-
-gulp.task('jshint:src', function (done) {
-  return gulp.src('./src/*.js')
-    .pipe($.jshint())
-    .pipe($.jshint.reporter('jshint-stylish'))
-    .pipe($.jshint.reporter('fail'));
-});
-
-gulp.task('jshint:test', function (done) {
-  return gulp.src('./test/*.spec.js')
-    .pipe($.jshint({
-      globals: {
-        "angular"    : false,
-        "module"    : false,
-        "inject"    : false,
-        "_jQuery"    : false,
-        "browserTrigger"    : false,
-
-        "jasmine"    : false,
-        "it"         : false,
-        "iit"        : false,
-        "xit"        : false,
-        "describe"   : false,
-        "ddescribe"  : false,
-        "xdescribe"  : false,
-        "dump"      : false,
-        "beforeEach" : false,
-        "afterEach"  : false,
-        "expect"     : false,
-        "spyOn"      : false
-      }
-    }))
-    .pipe($.jshint.reporter('jshint-stylish'))
-    .pipe($.jshint.reporter('fail'));
-});
-
-gulp.task('jshint', ['jshint:src', 'jshint:test']);
-
-//////////////////////////////////////////////////////////////////////////////
-// MINIFYING
-//////////////////////////////////////////////////////////////////////////////
-
-
-
-gulp.task('uglify', ['ng-annotate'], function () {
-  return gulp.src('./dist/*.js')
-    .pipe($.changed('./dist'))
-    .pipe($.rename({ suffix: '.min'}))
-    .pipe($.uglify({mangle: false}))
-    .pipe(gulp.dest('./dist'));
+gulp.task('dist', function() {
+  run('ng-factory:clean(dist)', ['ng-factory:templates(dist)', 'ng-factory:scripts(dist)']);
 });
 
 
