139060cf3aff6391cc891004fb8ce31d39cf8afc
Olivier Louvignes <olivier@mg-crea.com>
2014-08-07 13:01:49 +0200

    chore(ngdoc): add task (wip)

--- ng-factory/tasks/scripts/docs.js
+++ ng-factory/tasks/scripts/docs.js
@@ -1,23 +1,29 @@
 'use strict';
 
 var gulp = require('gulp');
-var config = require('./../../config'), docs = config.docs;
+var config = require('./../../config'), docs = config.docs, src = config.src;
 var pkg = require(process.cwd() + '/package.json');
 var path = require('path');
 
 var changed = require('gulp-changed');
 var connect = require('gulp-connect');
 
-var template = config.requireTransform('template');
+var nunjucks = config.requireTransform('nunjucks');
+var ngdoc = require('ngdoc-parser');
+var debug = config.requireTransform('debug');
 
 var cwd = path.resolve(__dirname, '..', '..', docs.cwd);
 
 gulp.task('ng-factory:scripts/docs(tmp)', function() {
 
-  var views = gulp.src(docs.scripts, {cwd: cwd, base: cwd})
+  var scripts = gulp.src(docs.scripts, {cwd: cwd, base: cwd})
     .pipe(changed(docs.tmp))
-    .pipe(template({locals: {pkg: pkg}, strict: true, rename: true}))
+    .pipe(nunjucks({locals: {pkg: pkg}, strict: true, rename: true}))
     .pipe(gulp.dest(docs.tmp))
     .pipe(connect.reload());
+
+  var ngdocs = gulp.src(src.scripts, {cwd: src.cwd})
+    .pipe(ngdoc())
+    .pipe(debug())
 
 });
